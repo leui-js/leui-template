f1fbf75385a66e17f617521fe21033554dab4ec1
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-08-20 15:03:42 +0800

    feat: 模板化，引入模板变量

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'LeHR',
+  base: '<%=projectName %>',
+
   server: {
     port: 3000,
   },
